4e12dfcad4e8a0f550d7045743556321c40e4acd
yamayamaru <yama9621@sunspot.jp>
2023-07-03 22:02:36 +0900

    update

--- source_code_2023-07-02-0002/arduino_uno_r4_rgb_led02/arduino_uno_r4_rgb_led02.ino
+++ source_code_2023-07-02-0002/arduino_uno_r4_rgb_led02/arduino_uno_r4_rgb_led02.ino
@@ -1,5 +1,5 @@
 /*
-    注意：NEO_PIXEL_PINの番号は0から7まで指定できます。8以降はまだ未対応
+    注意：NEO_PIXEL_PINの番号に任意のピンを指定できるようにしました
 */
 
 #define NEO_PIXEL_PIN 2
@@ -19,7 +19,7 @@ int getPixelNum();
 void setup() {
   Serial.begin(115200);
 
-  initPixelColor(NEO_PIXEL_PIN, 12);
+  initPixelColor(NEO_PIXEL_PIN, PIXEL_N);
   for (int i = 0; i < PIXEL_N; i++) {
     setPixelColor(i, rgb_color(0, 0, 255));
   }
@@ -294,37 +294,25 @@ void showPixelColor() {
 }
 
 
-
-/*
-C:\Users\username\AppData\Local\Arduino15\packages\arduino\hardware\renesas_uno\1.0.1\variants\MINIMA\includes\ra\fsp\src\bsp\cmsis\Device\RENESAS\Include
-0:P301
-1:P302
-2:P105
-3:P104
-4:P103
-5:P102
-6:P106
-7:P107
-*/
-uint8_t pin01[8][2] = {{3, 1}, {3, 2}, {1, 5}, {1, 4}, {1, 3}, {1, 2}, {1, 6}, {1, 7}};
 R_PORT0_Type *r_port_n[10] = {R_PORT0, R_PORT1, R_PORT2, R_PORT3, R_PORT4, R_PORT5, R_PORT6, R_PORT7, R_PORT8, R_PORT9};
 
 void digitalWrite01(uint8_t pin_no, uint8_t value) {
   if (value != (uint8_t)0) {
-    r_port_n[pin01[pin_no][0]]->PODR = (1 << pin01[pin_no][1]) | (r_port_n[pin01[pin_no][0]]->PODR);
+    r_port_n[g_pin_cfg[pin_no].pin >> 8]->PODR = (1 << (g_pin_cfg[pin_no].pin & 0xff)) | (r_port_n[g_pin_cfg[pin_no].pin >> 8]->PODR);
   } else {
-    r_port_n[pin01[pin_no][0]]->PODR = ~((uint32_t)((uint32_t)1 << pin01[pin_no][1])) & r_port_n[pin01[pin_no][0]]->PODR;
+    r_port_n[g_pin_cfg[pin_no].pin >> 8]->PODR = ~((uint32_t)((uint32_t)1 << (g_pin_cfg[pin_no].pin & 0xff))) & (r_port_n[g_pin_cfg[pin_no].pin >> 8]->PODR);
   }
 }
+
 
 R_PORT0_Type *r_port_n_number;
 uint32_t r_port_n_pin_mask, r_port_n_pin_mask_not;
 __IOM uint16_t *r_port_n_podr01;
+
 void set_pin_no(uint8_t pin_no) {
-  r_port_n_number = r_port_n[pin01[pin_no][0]];
-  // r_port_n_pin_mask = pin01[pin_no][1];
-  r_port_n_pin_mask = 1 << (pin01[pin_no][1]);
-  r_port_n_pin_mask_not = ~((uint32_t)((uint32_t)1 << (pin01[pin_no][1])));
+  r_port_n_number = r_port_n[g_pin_cfg[pin_no].pin >> 8];
+  r_port_n_pin_mask = 1 << (g_pin_cfg[pin_no].pin & 0xff);
+  r_port_n_pin_mask_not = ~r_port_n_pin_mask;
   r_port_n_podr01 = &(r_port_n_number->PODR);
 }
 
@@ -340,3 +328,48 @@ inline void pin_clear(void) {
 //  *r_port_n_podr01 = r_port_n_pin_mask_not & *r_port_n_podr01;
 }
 
+// renesas_uno/1.0.1/variants/MINIMA/variant.cpp:
+// extern "C" const PinMuxCfg_t g_pin_cfg[] = {
+//   { BSP_IO_PORT_03_PIN_01,    P301   }, /* (0) D0  -------------------------  DIGITAL  */
+//   { BSP_IO_PORT_03_PIN_02,    P302   }, /* (1) D1  */
+//   { BSP_IO_PORT_01_PIN_05,    P105   }, /* (2) D2  */
+//   { BSP_IO_PORT_01_PIN_04,    P104   }, /* (3) D3~ */
+//   { BSP_IO_PORT_01_PIN_03,    P103   }, /* (4) D4  */
+//   { BSP_IO_PORT_01_PIN_02,    P102   }, /* (5) D5~ */
+//   { BSP_IO_PORT_01_PIN_06,    P106   }, /* (6) D6~ */
+//   { BSP_IO_PORT_01_PIN_07,    P107   }, /* (7) D7  */
+//   { BSP_IO_PORT_03_PIN_04,    P304   }, /* (8) D8  */
+//   { BSP_IO_PORT_03_PIN_03,    P303   }, /* (9) D9~  */
+//   { BSP_IO_PORT_01_PIN_12,    P112   }, /* (10) D10~ */
+//   { BSP_IO_PORT_01_PIN_09,    P109   }, /* (11) D11~ */
+//   { BSP_IO_PORT_01_PIN_10,    P110   }, /* (12) D12 */
+//   { BSP_IO_PORT_01_PIN_11,    P111   }, /* (13) D13 */
+//   { BSP_IO_PORT_00_PIN_14,    P014   }, /* (14) A0  --------------------------  ANALOG  */
+//   { BSP_IO_PORT_00_PIN_00,    P000   }, /* (15) A1  */
+//   { BSP_IO_PORT_00_PIN_01,    P001   }, /* (16) A2  */
+//   { BSP_IO_PORT_00_PIN_02,    P002   }, /* (17) A3  */
+//   { BSP_IO_PORT_01_PIN_01,    P101   }, /* (18) A4/SDA  */
+//   { BSP_IO_PORT_01_PIN_00,    P100   }, /* (19) A5/SCL  */
+//   { BSP_IO_PORT_05_PIN_00,    P500   }, /* (20) Analog voltage measure pin  */
+// };
+
+// renesas_uno/1.0.1/variants/MINIMA/includes/ra/fsp/src/bsp/mcu/all/bsp_io.h:    typedef enum e_bsp_io_port_pin_t
+// {
+//     BSP_IO_PORT_00_PIN_00 = 0x0000,    ///< IO port 0 pin 0
+//     BSP_IO_PORT_00_PIN_01 = 0x0001,    ///< IO port 0 pin 1
+//     BSP_IO_PORT_00_PIN_02 = 0x0002,    ///< IO port 0 pin 2
+//     BSP_IO_PORT_00_PIN_03 = 0x0003,    ///< IO port 0 pin 3
+//     BSP_IO_PORT_00_PIN_04 = 0x0004,    ///< IO port 0 pin 4
+//     BSP_IO_PORT_00_PIN_05 = 0x0005,    ///< IO port 0 pin 5
+//     BSP_IO_PORT_00_PIN_06 = 0x0006,    ///< IO port 0 pin 6
+// 
+//     ************************ 中略 *****************************
+// 
+//     BSP_IO_PORT_14_PIN_11 = 0x0E0B,    ///< IO port 14 pin 11
+//     BSP_IO_PORT_14_PIN_12 = 0x0E0C,    ///< IO port 14 pin 12
+//     BSP_IO_PORT_14_PIN_13 = 0x0E0D,    ///< IO port 14 pin 13
+//     BSP_IO_PORT_14_PIN_14 = 0x0E0E,    ///< IO port 14 pin 14 
+//     BSP_IO_PORT_14_PIN_15 = 0x0E0F,    ///< IO port 14 pin 15
+//   } bsp_io_port_pin_t;
+// 
+// 上位8bitがポート番号、下位8bitがそのポートのピンの番号
