c2c37949e73deaef4c255fc4a9632a386d70bc4d
yamayamaru <yama9621@sunspot.jp>
2024-06-30 22:31:59 +0900

    update

---
+++ source-code_2024-06-30/msxclib.h
@@ -0,0 +1,18 @@
+void screen(int md);
+void color(int fg, int bg, int bd);
+
+void inispr();
+void sprite();
+void putspr();
+void colspr();
+unsigned char gtstck();
+unsigned char gttrig();
+int  gtpad();
+unsigned char snsmat();
+void kilbuf();
+void gicini();
+void sound();
+int  sgn();
+void srnd();
+
+
